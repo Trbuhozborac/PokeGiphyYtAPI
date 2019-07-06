b3ab4caa5c3d1a920bc315fc686a76f3dc2abff7
pomorange <nemanjapipi@hotmail.com>
2019-07-06 16:17:35 +0200

    giphyAPI finished

---
+++ GiiphyAPI/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using GiiphyAPI.Models;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GiiphyAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            string apiKey = System.Web.Configuration.WebConfigurationManager.AppSettings["giphyAPIKey"];
+            string query = "funny cat";
+
+            //Create the request to the API
+            WebRequest request = WebRequest.Create("http://api.giphy.com/v1/gifs/search?q=" + query+"&api_key=" + apiKey);
+
+            //Send that request off
+            WebResponse respone = request.GetResponse();
+
+            //Get back the response stram
+            Stream stream = respone.GetResponseStream();
+
+            //Make it accessible
+            StreamReader reader = new StreamReader(stream);
+
+            //Put into string , which is json formatted
+            string resposneFromServer = reader.ReadToEnd();
+
+            JObject parsedString = JObject.Parse(resposneFromServer);
+
+            Gif myGifs = parsedString.ToObject<Gif>();
+
+            return View(myGifs);
+        }
+    }
+}
---
+++ GiiphyAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GiiphyAPI.MvcApplication" Language="C#" %>
